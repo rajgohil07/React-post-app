88fc18e32e13022112c3a11a2da8119d655daf2e
rajgohil07 <raj.famous007@gmail.com>
2022-06-20 18:27:28 +0530

    added show more and less functionality

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,33 +9,52 @@ import "./Blog.css";
 export const Blog = () => {
   const getData = async () => {
     try {
+      let data = [];
       const config = {
         method: "GET",
         url: "https://jsonplaceholder.typicode.com/posts",
       };
-      const { data } = await axios(config);
-      return data;
+      ({ data } = await axios(config));
+      // get only 11 data from the server
+      data = data.slice(0, 11);
+      console.log("data.length :>> ", data.length);
+      return data.map((singularPostData) => ({
+        ...singularPostData,
+        author: "Raj gohil",
+      }));
     } catch (e) {
       throw e;
     }
   };
 
+  // states
+  const [getShowMore, setShowMore] = useState(false);
   const [getPost, setPost] = useState([]);
 
   const setSateFromServerData = async () => {
     const data = await getData();
     setPost(data);
   };
+
   useEffect(() => {
     (async () => await setSateFromServerData())();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const dataArray = getShowMore ? getPost : getPost.slice(0, 4);
+
   return (
     <div>
       <section className="Posts">
-        {getPost.map((singularData) => (
+        {dataArray.map((singularData) => (
           <Post key={singularData.id} title={singularData.title} />
         ))}
+        <br />
+      </section>
+      <section className="showMore">
+        <p onClick={() => setShowMore(!getShowMore)}>
+          {getShowMore ? "show less..." : "show more..."}
+        </p>
       </section>
       <section>
         <FullPost />
