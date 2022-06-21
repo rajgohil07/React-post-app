6afa12a79c71c0d89449b8726a4259040aad0356
rajgohil07 <raj.famous007@gmail.com>
2022-06-21 18:17:35 +0530

    add post

--- src/components/NewPost/NewPost.tsx
+++ src/components/NewPost/NewPost.tsx
@@ -1,34 +1,63 @@
-import React, { useState } from "react";
+import axios, { AxiosRequestConfig } from "axios";
+import { useState } from "react";
+import { Loading } from "../Loading/Loading";
 
 export const NewPost = () => {
   const [getTitle, setTitle] = useState("");
   const [getContent, setContent] = useState("");
-  const [getAuthor, setAuthor] = useState("max");
+  const [getAuthor, setAuthor] = useState("Raj");
+  const [getLoading, setLoading] = useState(false);
+
+  const createPost = async () => {
+    try {
+      setLoading(true);
+      const data = { title: getTitle, body: getContent, userId: 1 };
+      const config: AxiosRequestConfig = {
+        data,
+        method: "POST",
+        url: "https://jsonplaceholder.typicode.com/posts",
+      };
+      const { data: response } = await axios(config);
+      setLoading(false);
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  };
 
   return (
-    <div className="NewPost">
-      <h1>Add a Post</h1>
-      <label>Title</label>
-      <input
-        type="text"
-        value={getTitle}
-        onChange={(event) => setTitle(event.target.value)}
-      />
-      <label>Content</label>
-      <textarea
-        rows={4}
-        value={getContent}
-        onChange={(event) => setContent(event.target.value)}
-      />
-      <label>Author</label>
-      <select
-        value={getAuthor}
-        onChange={(event) => setAuthor(event.target.value)}
-      >
-        <option value="Max">Max</option>
-        <option value="Manu">Manu</option>
-      </select>
-      <button>Add Post</button>
+    <div>
+      <Loading display={getLoading} />
+      <div className="NewPost">
+        <h1>Add a Post</h1>
+        <label>Title</label>
+        <input
+          type="text"
+          value={getTitle}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Content</label>
+        <textarea
+          rows={4}
+          value={getContent}
+          onChange={(event) => setContent(event.target.value)}
+        />
+        <label>Author</label>
+        <select
+          value={getAuthor}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="Raj">Raj</option>
+          <option value="Manu">Manu</option>
+        </select>
+        <button
+          className="disabledButton"
+          disabled={!getContent || !getTitle}
+          onClick={createPost}
+        >
+          Add Post
+        </button>
+      </div>
     </div>
   );
 };
