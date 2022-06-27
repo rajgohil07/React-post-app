3b43e8355675023c18fc6afc1510a47177f48478
rajgohil07 <raj.famous007@gmail.com>
2022-06-27 19:42:12 +0530

    testing for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { Blog } from "./containers/Blog/Blog";
 
-const App = () => (
-  <BrowserRouter>
-    <div className="App">
-      <Blog />
-    </div>
-  </BrowserRouter>
-);
+const App = () => {
+  const baseURL =
+    process.env.NODE_ENV !== "production" ? "/" : "/React-post-app";
+  console.log(baseURL);
+  console.log("process.env.NODE_ENV :>> ", process.env.NODE_ENV);
+  return (
+    <BrowserRouter basename={baseURL}>
+      <div className="App">
+        <Blog />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
